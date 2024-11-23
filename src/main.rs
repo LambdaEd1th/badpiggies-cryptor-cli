0b6ac3455bb7ff8115b208cf191d092f433ce63c
ed1th <wang991013@hotmail.com>
2024-11-23 23:25:52 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result<(), Error> {
     match cli.command {
         Commands::Encrypt(args) => {
             let mut input_file = File::open(args.input_file)?;
-            let mut input_file_buffer: Vec<u8> = Vec::new();
+            let mut input_file_buffer = Vec::<u8>::new();
             input_file.read_to_end(&mut input_file_buffer)?;
             let cryptor = Cryptor::new(&input_file_buffer);
             let output_buffer;
@@ -37,7 +37,7 @@ fn main() -> Result<(), Error> {
         }
         Commands::Decrypt(args) => {
             let mut input_file = File::open(args.input_file)?;
-            let mut input_file_buffer: Vec<u8> = Vec::new();
+            let mut input_file_buffer = Vec::<u8>::new();
             input_file.read_to_end(&mut input_file_buffer)?;
             let cryptor = Cryptor::new(&input_file_buffer);
             let output_buffer;
